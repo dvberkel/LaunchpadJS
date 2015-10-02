1f34eedffa679d575d680bcf971f66cfa904ab2f
dvberkel <daan.v.berkel.1980@gmail.com>
2015-10-02 14:59:52 +0200

    Create a uglified version of the source

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-gulp.task('default', function(){
+var dist_dir = './'
+var dist_file = 'launchpad.js';
+
+gulp.task('concat', function(){
     return gulp.src(['src/launchpad.js'])
-        .pipe(concat('launchpad.js', { newLine: ';' }))
-        .pipe(gulp.dest('./'));
+        .pipe(concat(dist_file, { newLine: ';' }))
+        .pipe(gulp.dest(dist_dir));
+});
+
+gulp.task('compress', ['concat'], function(){
+    return gulp.src(dist_file)
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest(dist_dir));
+});
+
+gulp.task('default', ['compress'], function(){
+    /* noting to do here */
 });
