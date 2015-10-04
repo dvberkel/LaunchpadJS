1bfee8ad6a4e4eca53c15bef641546cda9c459d9
dvberkel <daan.v.berkel.1980@gmail.com>
2015-10-04 07:29:13 +0200

    Check for launchpad version

--- spec/launchpadSpec.js
+++ spec/launchpadSpec.js
@@ -2,4 +2,8 @@ describe('launchpad', function(){
     it('should exist', function(){
         expect(launchpad).toBeDefined();
     });
+
+    it('should have a version', function(){
+        expect(launchpad.version).toBeDefined();
+    });
 });
