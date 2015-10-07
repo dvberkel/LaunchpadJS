011881e40734be450ace15366d436b62d2fd7a2b
dvberkel <daan.v.berkel.1980@gmail.com>
2015-10-07 07:11:32 +0200

    Observable should notify all registered observers

--- spec/observerSpec.js
+++ spec/observerSpec.js
@@ -6,7 +6,6 @@ describe('observerable', function(){
         this.notified = true;
     }
 
-
     it('should register callbacks specific for events', function(){
         var observer = new Observer();
         var otherObserver= new Observer();
@@ -17,5 +16,24 @@ describe('observerable', function(){
 
         expect(observer.notified).toBe(true);
         expect(otherObserver.notified).toBe(false);
+    });
+
+    it('should notify all observers for an event', function(){
+        var observers = [1, 2, 3].map(function(id){
+            var observer = new Observer();
+            observer.id = id;
+            return observer;
+        });
+        var observable = new launchpad.Observable();
+        observers.forEach(function(observer){
+            observable.on('test', observer.callback.bind(observer));
+        });
+
+        observable.emit('test');
+
+        var allNotified = observers.reduce(function(allNotified, observer){
+            return allNotified && observer.notified;
+        }, true);
+        expect(allNotified).toBe(true);
     });
 });
