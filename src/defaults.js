c3d8eb28ec45860d4bc1d796f67547db192574d3
dvberkel <daan.v.berkel.1980@gmail.com>
2015-10-11 12:48:29 +0200

    Create default midiAdapterFactory

--- src/defaults.js
+++ src/defaults.js
@@ -1,14 +1,45 @@
 ;(function($, undefined){
     var defaults = $.defaults = {};
 
+    function selectItem(name){
+        return {
+            'from': function(items){
+                var item = items.next();
+                while (!item.done) {
+                    if (item.value.name === name) {
+                        return item.value;
+                    }
+                    item = items.next();
+                }
+                return undefined;
+            }
+        }
+    };
+
     defaults.name = 'Launchpad Mini';
     defaults.sysex = false;
     defaults.midiAdapterFactory = function(accept, reject){
         if (!navigator.requestMIDIAccess){
             reject();
         } else {
-            /* TODO make a real midi adapter */
-            accept(undefined);
+            var padName = this.name;
+            navigator.requestMIDIAccess({ sysex: this.sysex })
+                .then(function(midiAccess){
+                    return {
+                        'input': selectItem(padName).from(midiAccess.inputs.values()),
+                        'output': selectItem(padName).from(midiAccess.outputs.values())
+                    }
+                })
+                .then(function(io){
+                    return new $.MidiAdapter(io.input, io.output);
+                })
+                .then(function(midiAdapter){
+                    return new $.Launchpad(midiAdapter);;
+                })
+                .then(function(pad){
+                    accept(pad);
+                })
+                .catch(reject);
         }
     };
 })(window.launchpad = window.launchpad || {});
